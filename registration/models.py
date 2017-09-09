cf913ca3f6e21b1cba8d39decab3214cefe96efa
Norton Pengra <npengra317@gmail.com>
2017-09-09 02:47:22 -0700

    replace teacher model with user

--- registration/models.py
+++ registration/models.py
@@ -1,22 +1,15 @@
 from django.db import models
 from django import forms
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class School(models.Model):
     "An object representing a single school."
     name = models.CharField(max_length=255, unique=True)
-    primary_contact = models.OneToOneField('Teacher', related_name='primary_contact_for', null=True)
+    primary_contact = models.OneToOneField(User, related_name='primary_contact_for', null=True)
     auth_code = models.CharField(max_length=32, unique=True)
     school_image = models.URLField()
 
     def __str__(self):
         return self.name
-
-class Teacher(models.Model):
-    "An account that allows a librarian/Teacher to view their school data."
-    school = models.ForeignKey(School)
-    email = models.EmailField()
-    password = models.CharField(max_length=32)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
