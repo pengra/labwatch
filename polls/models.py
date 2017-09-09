d7b46872aac0d57619ac33a0633b5f7544b9002e
Norton Pengra <npengra317@gmail.com>
2017-09-08 19:11:55 -0700

    create poll models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class PollQuestion(models.Model):
+    "A question model for asking students questions."
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class PollChoice(models.Model):
+    "A choice for a question."
+    question = models.ForeignKey(PollQuestion, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
