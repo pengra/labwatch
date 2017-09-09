edad89c869728b1174d45227000d0f5f195ef6f3
Norton Pengra <npengra317@gmail.com>
2017-09-09 02:30:50 -0700

    define view for login page

--- index/views.py
+++ index/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def cover_view(request):
     "Primary view."
     return render(request, 'index/coverpage.html')
+
+def login_view(request):
+    "Login for everyone view."
+    return render(request, 'index/login.html')
