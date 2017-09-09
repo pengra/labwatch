f0f0dbd931065374d91191ce1ef0de59c20e8570
Norton Pengra <npengra317@gmail.com>
2017-09-09 03:15:19 -0700

    create autehtnication form

--- index/views.py
+++ index/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
 
 def cover_view(request):
     "Primary view."
     return render(request, 'index/coverpage.html')
 
+
 def login_view(request):
     "Login for everyone view."
-    return render(request, 'index/login.html')
+
+    # If the user is already authenticated
+    # take them to the home page
+    if request.user.is_authenticated:
+        return redirect('index:index')
+
+    context = {}
+
+    # handle authentication requests via django auth.login/authenticate
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Upon succesful authentication, redirect to homepage
+            return redirect('index:index')
+        else:
+            # Change context upon invalid login
+            context = {
+                "error": "Invalid Credentials"
+            }
+
+    # If made it all the way to the end, spew out
+    # login form with context.
+    return render(request, 'index/login.html', context)
