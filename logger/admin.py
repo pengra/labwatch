f496916b89f395218fd0762274830083c6021bf2
Norton Pengra <npengra317@gmail.com>
2017-09-08 19:37:57 -0700

    register admin pages for logger models

--- logger/admin.py
+++ logger/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from logger import models
 
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+class LogAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Log, LogAdmin)
+admin.site.register(models.Student, StudentAdmin)
