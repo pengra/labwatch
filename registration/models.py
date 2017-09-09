ef388fa5741744842e610b717d577d3f498c36cf
Norton Pengra <npengra317@gmail.com>
2017-09-08 18:44:09 -0700

    add school model

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    "An object representing a single school."
+    name = models.CharField(max_length=255, unique=True)
+    contact_email = models.EmailField()
+    auth_code = models.CharField(max_length=32, unique=True)
+    school_image = models.URLField()
