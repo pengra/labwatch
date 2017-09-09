7cb2bee8f62aca68d17dd84a2e7bdc04b355e5d2
Norton Pengra <npengra317@gmail.com>
2017-09-09 13:22:06 -0700

    only deal with postgres when debugmode is false

--- labwatch/settings.py
+++ labwatch/settings.py
@@ -89,8 +89,9 @@ DATABASES = {
 }
 
 # Set up database for Heroku
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+if not DEBUG:
+    db_from_env = dj_database_url.config(conn_max_age=500)  
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
