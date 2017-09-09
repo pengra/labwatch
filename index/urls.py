8ec48b624f409654d7634e1e1a9c8f4640b81d85
Norton Pengra <npengra317@gmail.com>
2017-09-09 04:49:56 -0700

    fix all url included bug

--- index/urls.py
+++ index/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'login/$', views.login_view, name='login'),
     url(r'dashboard/kiosk/$', views.dashboard_kiosk_view, name='dashboard-kiosk'),
     url(r'dashboard/$', views.dashboard_view, name='dashboard'),
-    url(r'$', views.cover_view, name='index'),
+    url(r'^$', views.cover_view, name='index'),
 ]
