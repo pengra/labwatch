ffc16667a1a7cb0c996a8a4e74f2fbd06ae852c9
Norton Pengra <npengra317@gmail.com>
2017-09-09 13:30:15 -0700

    add custom commands to manage for quick deploys

--- manage.py
+++ manage.py
@@ -19,4 +19,21 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    execute_from_command_line(sys.argv)
+    if (sys.argv[1]) == 'autocreatesuperuser':
+        from django.contrib.auth.models import User
+        User.objects.filter(email=os.environ['SUPERUSER_EMAIL']).delete()
+        User.objects.create_superuser(
+            os.environ['SUPERUSER_USERNAME'],
+            os.environ['SUPERUSER_EMAIL'],
+            os.environ['SUPERUSER_PASSWORD']
+        )
+    elif (sys.argv[1]) == 'autocreategroups':
+        from django.contrib.auth.models import Group
+        if len(Group.objects.filter(name='Engineer')) == 0:
+            Group('Engineer').save()
+        if len(Group.objects.filter(name='Librarian')) == 0:
+            Group('Librarian').save()
+        if len(Group.objects.filter(name='Teacher')) == 0:
+            Group('Teacher').save()
+    else:
+        execute_from_command_line(sys.argv)
