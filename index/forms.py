e6c13b86751970feafd31693867a89450340a162
Norton Pengra <npengra317@gmail.com>
2017-09-10 15:24:36 -0700

    update forms to handle log_pk

--- index/forms.py
+++ index/forms.py
@@ -15,6 +15,7 @@ class LoginForm(forms.Form):
     "A form for an AJAX hit from a kiosk."
     mode = forms.CharField()
     return_value = forms.CharField()
+    log_pk = forms.IntegerField(required=False)
 
     def is_valid_card_number(self):
         "Is this a card number the user just typed in?"
@@ -33,18 +34,21 @@ class LoginForm(forms.Form):
 
     def is_valid_name(self):
         "Is this a valid name this person just typed in?"
-        is_one_space_min = len(self.cleaned_data.get('return_value', '').split(' ')) > 1
-        return self.is_valid_nickname() and is_one_space_min        
+        is_one_space_min = len(self.cleaned_data.get(
+            'return_value', '').split(' ')) > 1
+        return self.is_valid_nickname() and is_one_space_min
 
     def is_valid_nickname(self):
         "is this a valid nickname this person just yped in?"
         is_valid = self.is_valid()
         is_box = self.cleaned_data.get('mode') == '_box'
-        is_text = self.cleaned_data.get('return_value', '1').replace(' ', '').replace('-', '').isalpha()
+        is_text = self.cleaned_data.get('return_value', '1').replace(
+            ' ', '').replace('-', '').isalpha()
         return is_valid and is_box and is_text
 
     def is_valid_poll(self):
         "Is this a valid poll option coming through?"
         is_valid = self.is_valid()
         is_poll = self.cleaned_data.get('mode') == '_poll'
-        return is_valid and is_poll
+        contains_pk = 'log_pk' in self.data
+        return is_valid and is_poll and contains_pk
