6e2deb4e9302b8368f6293c043e0f350db480ef8
Norton Pengra <npengra317@gmail.com>
2017-09-10 01:58:22 -0700

    new route for kiosk poll post endpoint

--- index/urls.py
+++ index/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'dashboard/kiosk/$', views.dashboard_kiosk_view, name='dashboard-kiosk'),
     url(r'dashboard/$', views.dashboard_view, name='dashboard'),
     url(r'kiosk/(?P<auth_code>[a-zA-Z0-9]+)/$', views.kiosk_view, name='kiosk-page'),
+    url(r'kiosk/(?P<auth_code>[a-zA-Z0-9]+)/poll/$', views.kiosk_poll_view, name='kiosk-poll'),
     url(r'^$', views.cover_view, name='index'),
 ]
