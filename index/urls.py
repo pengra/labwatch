4cdd2f2bc416acc2fabefab02f504a6c3a5f5a2c
Norton Pengra <npengra317@gmail.com>
2017-09-10 16:08:25 -0700

    new endpoint for polls

--- index/urls.py
+++ index/urls.py
@@ -4,6 +4,7 @@ from index import views
 urlpatterns = [
     url(r'login/$', views.login_view, name='login'),
     url(r'dashboard/kiosk/$', views.dashboard_kiosk_view, name='dashboard-kiosk'),
+    url(r'dashboard/poll/$', views.dashboard_poll_view, name='dashboard-poll'),
     url(r'dashboard/$', views.dashboard_view, name='dashboard'),
     url(r'kiosk/(?P<auth_code>[a-zA-Z0-9]+)/$', views.kiosk_view, name='kiosk-page'),
     url(r'kiosk/(?P<auth_code>[a-zA-Z0-9]+)/poll/$', views.kiosk_poll_view, name='kiosk-poll'),
