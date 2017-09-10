f5530c11bd4e7c6ec58afdb305db140cd1cfa224
Norton Pengra <npengra317@gmail.com>
2017-09-09 19:15:47 -0700

    tie poll to kiosk

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from registration.models import Kiosk
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class PollQuestion(models.Model):
     "A question model for asking students questions."
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    kiosk = models.ManyToManyField(Kiosk)
 
     def __str__(self):
         return self.question_text
