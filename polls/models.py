e39a91ed54a3286de4e6dad9adf4661f6afa4ca3
Norton Pengra <npengra317@gmail.com>
2017-09-10 20:15:11 -0700

    linting

--- polls/models.py
+++ polls/models.py
@@ -3,14 +3,16 @@ from registration.models import Kiosk
 
 # Create your models here.
 
+
 class PollQuestion(models.Model):
     "A question model for asking students questions."
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now=True)
     kiosk = models.ManyToManyField(Kiosk)
 
     def __str__(self):
         return self.question_text
+
 
 class PollChoice(models.Model):
     "A choice for a question."
