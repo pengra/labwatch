eb1b05874a65e9d9f08cf010e40244e75f7da6ce
Norton Pengra <npengra317@gmail.com>
2017-09-13 00:40:53 -0700

    replace student spreadsheet upload with class view

--- index/views.py
+++ index/views.py
@@ -411,20 +411,13 @@ class DashboardPollView(LoginRequiredMixin, BaseLabDashView):
 
         return render(request, 'dashboard/poll.html', context)
 
-
-def dashboard_student_bulk_view(request):
-    "view for uploading students by excel sheet."
-    context = {
-        'is_librarian': len(request.user.groups.filter(name__in=['Librarian'])),
-        'is_engineer': len(request.user.groups.filter(name__in=['Engineer'])),
-    }
-    if request.user.is_authenticated():
-        school = request.user.associated_school.get()
-        context['school'] = school
-
+class DashboardStudentBulkView(LoginRequiredMixin, BaseLabDashView):
+    
+    def get(self, request):
+        "view for uploading students by excel sheet."
+        context = self.get_context(request)
         return render(request, 'dashboard/bulk.html', context)
 
-    return redirect('index:login')
 
 
 def dashboard_student_admin_view(request):
