166d656c964f5ed70ffa0800c902532193669adf
Norton Pengra <npengra317@gmail.com>
2017-09-13 11:13:38 -0700

    make debug env var case insensitive

--- labwatch/settings.py
+++ labwatch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', 'def a secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = [
     'lab-watch.herokuapp.com',
