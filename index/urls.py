f506b2bc1dc94908862b4c98a6c823d695fb4eff
Norton Pengra <npengra317@gmail.com>
2017-09-13 16:07:28 -0700

    new report endpoint

--- index/urls.py
+++ index/urls.py
@@ -4,9 +4,10 @@ from index import views
 urlpatterns = [
     url(r'login/$', views.LoginView.as_view(), name='login'),
     url(r'dashboard/students/upload-spreadsheet/$', views.DashboardStudentBulkView.as_view(), name='dashboard-student-bulk'),
-    url(r'dashboard/students/manage/$', views.DashboardStudentAdminView.as_view(), name='dashboard-student'),
+    url(r'dashboard/students/$', views.DashboardStudentAdminView.as_view(), name='dashboard-student'),
     url(r'dashboard/kiosk/$', views.DashboardKioskView.as_view(), name='dashboard-kiosk'),
     url(r'dashboard/poll/$', views.DashboardPollView.as_view(), name='dashboard-poll'),
+    url(r'dashboard/report/$', views.DashboardReportsView.as_view(), name='dashboard-reports'),
     url(r'dashboard/$', views.DashboardView.as_view(), name='dashboard'),
     url(r'kiosk/(?P<auth_code>[a-zA-Z0-9]+)/$', views.KioskView.as_view(), name='kiosk-page'),
     url(r'kiosk/(?P<auth_code>[a-zA-Z0-9]+)/poll/$', views.KioskPollView.as_view(), name='kiosk-poll'),
