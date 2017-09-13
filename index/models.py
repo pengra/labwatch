328f507c8bfe5a8647302bbe2ea95de32b21d086
Norton Pengra <npengra317@gmail.com>
2017-09-13 02:50:46 -0700

    attach user to each report

--- index/models.py
+++ index/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserReport(models.Model):
     "Model for bug reports/questions."
@@ -12,6 +13,7 @@ class UserReport(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     dealt_with = models.BooleanField(default=False)
+    user = models.ForeignKey(User, null=True)
 
     def __str__(self):
         return self.title
