a8ca90f64946a88ec5d4037de2734914e28d8479
Norton Pengra <npengra317@gmail.com>
2017-09-15 11:40:12 -0700

    new student search form

--- index/forms.py
+++ index/forms.py
@@ -95,3 +95,34 @@ class StudentCreationForm(forms.Form):
     crud_teacher = forms.CharField(required=False)
     crud_grade = forms.CharField(required=False)
     crud_email = forms.EmailField(required=False)
+
+
+class StudentSearchForm(forms.Form):
+    "Form for finding students."
+    search_target = forms.CharField()
+
+    def is_valid_card_number(self):
+        "Is this a card number the user just typed in?"
+        is_valid = self.is_valid()
+        is_numeric = self.cleaned_data.get('search_target', '').isdigit()
+        return is_valid and is_numeric
+
+    def is_valid_email(self):
+        "Is this an email this person just typed in?"
+        is_valid = self.is_valid()
+        contains_at = "@" in self.cleaned_data.get('search_target', '')
+        contains_period = '.' in self.cleaned_data.get('search_target', '')
+        return is_valid and contains_at and contains_period
+
+    def is_valid_name(self):
+        "Is this a valid name this person just typed in?"
+        is_one_space_min = len(self.cleaned_data.get(
+            'search_target', '').split(' ')) > 1
+        return self.is_valid_nickname() and is_one_space_min
+
+    def is_valid_nickname(self):
+        "is this a valid nickname this person just yped in?"
+        is_valid = self.is_valid()
+        is_text = self.cleaned_data.get('search_target', '1').replace(
+            ' ', '').replace('-', '').isalpha()
+        return is_valid and is_text
