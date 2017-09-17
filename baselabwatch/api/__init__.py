15b545efab44e9e35f83aab3f81a1ac07affcb76
qwergram <npengra317@gmail.com>
2017-09-17 04:39:52 -0700

    register new view sets

--- baselabwatch/api/__init__.py
+++ baselabwatch/api/__init__.py
@@ -1,2 +1,6 @@
 from baselabwatch.api.user import UserViewSet
 from baselabwatch.api.profile import ProfileViewSet
+from baselabwatch.api.school import SchoolViewSet
+from baselabwatch.api.student import StudentViewSet
+from baselabwatch.api.subscription import SubscriptionViewSet
+from baselabwatch.api.userreport import UserReportViewSet
