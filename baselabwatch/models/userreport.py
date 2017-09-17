36f8c0f107c0a4c6f3f1225fb8f0b1adb16cdae6
qwergram <npengra317@gmail.com>
2017-09-17 04:39:27 -0700

    adjust report type titles

--- baselabwatch/models/userreport.py
+++ baselabwatch/models/userreport.py
@@ -5,11 +5,11 @@ class UserReport(models.Model):
     "Model for bug reports/questions."
 
     REPORT_TYPES = (
-        ('bug', 'Question'),
+        ('bug', 'Bug'),
         ('question', 'Question'),
     )
 
-    report_type = models.CharField(max_length=255, choices=REPORT_TYPES)
+    report_type = models.CharField(max_length=10, choices=REPORT_TYPES)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     dealt_with = models.BooleanField(default=False)
