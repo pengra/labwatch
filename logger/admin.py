3483132599095e3041720e3b32fb51eead46d0be
qwergram <npengra317@gmail.com>
2017-09-17 14:19:30 -0700

    add all models to admin site

--- logger/admin.py
+++ logger/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from logger import models
+
 
 # Register your models here.
+admin.site.register(models.StudentSession)
+admin.site.register(models.Kiosk)
+admin.site.register(models.PollQuestion)
+admin.site.register(models.PollChoice)
+admin.site.register(models.ImageCard)
