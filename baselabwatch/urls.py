f6079271db07c5a49ae6d0ccdf43aea968e96225
qwergram <npengra317@gmail.com>
2017-09-17 04:39:05 -0700

    new endpoints for new objects

--- baselabwatch/urls.py
+++ baselabwatch/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from baselabwatch.api import UserViewSet, ProfileViewSet
+from baselabwatch import api
 
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'profiles', ProfileViewSet)
+router.register(r'users', api.UserViewSet)
+router.register(r'profiles', api.ProfileViewSet)
+router.register(r'schools', api.SchoolViewSet)
+router.register(r'students', api.StudentViewSet)
+router.register(r'subscriptions', api.SubscriptionViewSet)
+router.register(r'reports', api.UserReportViewSet)
+
 
 urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
