c9bf204671d4d02a51fbafb26b00a881323e4928
qwergram <npengra317@gmail.com>
2017-09-16 18:12:58 -0700

    import models in __init__

---
+++ baselabwatch/models/__init__.py
@@ -0,0 +1,5 @@
+from baselabwatch.models.profile import Profile
+from baselabwatch.models.school import School
+from baselabwatch.models.student import Student
+from baselabwatch.models.subscription import Subscription
+from baselabwatch.models.userreport import UserReport
