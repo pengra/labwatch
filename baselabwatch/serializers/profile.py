edec0516115b1a5fd930300c1c021853251493dc
qwergram <npengra317@gmail.com>
2017-09-17 04:08:30 -0700

    add school field to viewset

--- baselabwatch/serializers/profile.py
+++ baselabwatch/serializers/profile.py
@@ -5,14 +5,14 @@ from baselabwatch.models import Profile, School
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     "Serializer for user."
     user = serializers.HyperlinkedRelatedField(source="user.pk", view_name='user-detail', queryset=User.objects.all())
-    # school = serializers.HyperlinkedRelatedField(source="school", view_name='school-detail', queryset=School.objects.all())
+    school = serializers.HyperlinkedRelatedField(source="school.pk", view_name='school-detail', queryset=School.objects.all())
 
     class Meta:
         model = Profile
         fields = (
             'url',
             'user',
-            # 'school',
+            'school',
             'engineer',
             'librarian',
             'techsavy',
