b482d970fbdf29888ce49461e9a5507751a7c9db
qwergram <npengra317@gmail.com>
2017-09-17 15:46:01 -0700

    two new views

--- baselabwatch/views/index.py
+++ baselabwatch/views/index.py
@@ -2,9 +2,3 @@ from django.views.generic import View, TemplateView
 
 class IndexView(TemplateView):
     template_name = "base/coverpage.html"
-
-class LoginView(TemplateView):
-    template_name = 'base/login.html'
-
-class SignupView(TemplateView):
-    template_name = 'base/signup.html'
