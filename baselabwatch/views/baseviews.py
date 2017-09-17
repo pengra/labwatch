f867a141cde1a45504d54faaf03a1ee63143ea53
qwergram <npengra317@gmail.com>
2017-09-17 15:45:52 -0700

    remove useless view

--- baselabwatch/views/baseviews.py
+++ baselabwatch/views/baseviews.py
@@ -7,17 +7,3 @@ contexts relating to user:
 
 from django.views.generic import View
 
-
-class BaseLabDashView(View):
-    "Use this as basic class for context generation for all pages."
-    # used for LoginRequiredMixin
-    login_url = '/login/'
-
-    def get_context(self, request):
-        "overall context for all views method."
-        context = {
-            'school': None
-        }
-        if request.user.is_authenticated:
-            context['school'] = request.user.profile.school
-        return context
