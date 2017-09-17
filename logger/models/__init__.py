2e4e69a5cace93b771d1358089ec98495f2c32d2
qwergram <npengra317@gmail.com>
2017-09-17 14:05:32 -0700

    new session object for logging

---
+++ logger/models/__init__.py
@@ -0,0 +1,1 @@
+from logger.models.session import StudentSession
---
+++ logger/models/session.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from baselabwatch.models import Student
+# Create your models here.
+
+class StudentSession(models.Model):
+    "A log of a student login/logout."
+
+    # Models actually depend on these indexes now
+    # Do not change.
+
+    INPUT_MODE = (
+        ('CARD', 'Card Scan'),  # 0
+        ('NAME', 'Name input'),  # 1
+        ('EMAL', 'Email'),  # 2
+        ('NICK', 'Nickname input'),  # 3
+        ('ADMI', 'Admin input')  # 4
+    )
+
+    student = models.ForeignKey(Student)
+
+    sign_in_mode = models.CharField(max_length=3, choices=INPUT_MODE)
+    sign_out_mode = models.CharField(max_length=3, choices=INPUT_MODE)
+
+    sign_in_timestamp = models.DateTimeField(default=timezone.now)
+    sign_out_timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return "{}: {}".format(self.student, self.mode)
