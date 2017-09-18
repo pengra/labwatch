689663822c4b3bfc5d5b0256603c4a6b59f9bb92
qwergram <npengra317@gmail.com>
2017-09-17 18:01:31 -0700

    new title bar components designed

--- baselabwatch/static/base/jsx/base.jsx
+++ baselabwatch/static/base/jsx/base.jsx
@@ -1,4 +1,27 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('main-content-pane')
-);
+class TitleNavBarLink extends React.Component {
+  render() {
+    return (
+      <li className="nav-item">
+        <a className="nav-link" href="#">{this.props.text}</a>
+      </li>
+    )
+  }
+}
+
+class TitleNavBar extends React.Component {
+  render() {
+    const links = this.props.links;
+    let renderedLinks = Array(links.length);
+    for (let i = 0; i < links.length; i++) {
+      renderedLinks.push(
+        <TitleNavBarLink text={links[i]} key={i} />
+      )
+    }
+
+    return (
+      <ul className="navbar-nav mr-auto" id="title-nav-bar-list">
+        {renderedLinks}
+      </ul>
+    )
+  }
+}
