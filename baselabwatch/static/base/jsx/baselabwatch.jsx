b18591f2c20aacbc0e68519acf82f28a9ba0e13a
qwergram <npengra317@gmail.com>
2017-09-17 20:09:45 -0700

    working left nav tabs in react

--- baselabwatch/static/base/jsx/baselabwatch.jsx
+++ baselabwatch/static/base/jsx/baselabwatch.jsx
@@ -1,3 +1,12 @@
 const leftNavBarLinks = [
     "School", "Students", "Profile"
 ]
+
+class MainContent extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (<div><h1>Coming soon!</h1></div>)
+  }
+}
--- baselabwatch/static/base/jsx/navigation.jsx
+++ baselabwatch/static/base/jsx/navigation.jsx
@@ -32,27 +32,50 @@ class TitleNavBar extends React.Component {
 }
 
 class LeftNavBarLink extends React.Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    return this.props.handleClick(this.props.id);
+  }
   render() {
     let hrefClass = this.props.active ? "nav-link active" : "nav-link";
     return (
       <li className="nav-item">
-        <a className={hrefClass} href="#">{this.props.text}</a>
+        <a className={hrefClass} onClick={this.handleClick} href="#">{this.props.text}</a>
       </li>
     )
   }
 }
 
 class LeftNavBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeIndex: 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }  
+  handleClick(id) {
+    this.setState({
+      activeIndex: id
+    });
+    this.render();
+  }
   render() {
     const links = this.props.links;
     let renderedLinks = Array(links.length);
     for (let i = 0; i < links.length; i++) { 
-      const active = i === this.props.activeIndex;
+      const active = i === this.state.activeIndex;
       renderedLinks.push(
         <LeftNavBarLink 
           text={links[i]}
-          key={i} 
-          active={active}/>
+          key={i}
+          id={i}
+          active={active}
+          handleClick={this.handleClick}
+          />
       );
     }
     return (
