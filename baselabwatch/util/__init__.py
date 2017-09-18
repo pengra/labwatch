46a23e01b3bf9bde80f760c8f6b6f83cdaca9a94
qwergram <npengra317@gmail.com>
2017-09-17 18:25:31 -0700

    add more meta data to links

--- baselabwatch/util/__init__.py
+++ baselabwatch/util/__init__.py
@@ -1,14 +1,24 @@
 from labwatch.settings import LABWATCH_APPS
+from django.urls import reverse
+from django.core.urlresolvers import NoReverseMatch
 import importlib
 
 
-def get_title_links():
+def get_app_metadata():
     "Function that grabs all the title links."
-    links = []
+    meta_data = []
     for app in LABWATCH_APPS:
-        app = importlib.import_module("{}.apps".format(app))
-        links.append(app.ReactConfig.tab_title)
+        app_module = importlib.import_module("{}.apps".format(app))
+        try:
+            url = reverse("{}:index".format(app))
+        except NoReverseMatch:
+            url = '#'
+        meta_data.append({
+            "title": app_module.ReactConfig.tab_title,
+            "name": app,
+            "url": url,
+        })
         # Faster with del
         # Intel i7 timeit tests
         del app
-    return links
+    return meta_data
