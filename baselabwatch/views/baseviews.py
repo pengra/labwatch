1c91796a04f9349466778b2d241211cb07d5263e
qwergram <npengra317@gmail.com>
2017-09-17 18:02:00 -0700

    add title_links to all contexts

--- baselabwatch/views/baseviews.py
+++ baselabwatch/views/baseviews.py
@@ -6,9 +6,15 @@ contexts relating to user:
 """
 
 from django.views.generic import View, TemplateView 
+from baselabwatch.util import get_title_links
 
 class DashboardBase(TemplateView):
     template_name = 'base/_base.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['title_links'] = str(get_title_links())
+        return context
 
     def post(self, request, *args, **kwargs):
         "Handle post requests."
