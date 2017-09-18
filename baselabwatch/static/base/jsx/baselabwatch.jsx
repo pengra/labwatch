61761bf809ac11408fcfafcf30acb720e063d93a
qwergram <npengra317@gmail.com>
2017-09-17 22:15:02 -0700

    create dummy renders for each view

--- baselabwatch/static/base/jsx/baselabwatch.jsx
+++ baselabwatch/static/base/jsx/baselabwatch.jsx
@@ -1,5 +1,5 @@
 const leftNavBarLinks = [
-    "School", "Students", "Profile"
+  "School", "Students", "Profile"
 ]
 
 class MainContent extends LabWatchWebsite {
@@ -7,6 +7,8 @@ class MainContent extends LabWatchWebsite {
     super();
 
     this.renderSchoolPage = this.renderSchoolPage.bind(this);
+    this.renderStudentsPage = this.renderStudentsPage.bind(this);
+    this.renderProfilePage = this.renderProfilePage.bind(this);
   }
   renderSchoolPage() {
     const example = [
@@ -15,14 +17,33 @@ class MainContent extends LabWatchWebsite {
       ["Payment", <div>morecontent2</div>],
     ];
     return (<div>
-      <TabbedPage tabs={example}/>
+      <TabbedPage tabs={example} />
+    </div>)
+  }
+  renderStudentsPage() {
+    const example = [
+      ["Create", <div>content</div>],
+      ["Find/Edit", <div>morecontent</div>],
+      ["Upload", <div>morecontent2</div>],
+    ];
+    return (<div>
+      <TabbedPage tabs={example} />
+    </div>)
+  }
+  renderProfilePage() {
+    return (<div>
+      <h1>Edit your Profile</h1>
     </div>)
   }
   renderMainPage() {
     if (this.state.currentPage === 0) {
       return this.renderSchoolPage();
+    } else if (this.state.currentPage === 1) {
+      return this.renderStudentsPage();
+    } else if (this.state.currentPage === 2) {
+      return this.renderProfilePage();
     } else {
-      return (<h1>Something went wrong...</h1>)
+      return (<h1>This feature is currently under development.</h1>)
     }
   }
 }
