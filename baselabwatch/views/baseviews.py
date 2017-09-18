22cfcf1b053fdd2ad57264903b444ab0ea6af004
qwergram <npengra317@gmail.com>
2017-09-17 17:09:23 -0700

    copy template dashboardbase

--- baselabwatch/views/baseviews.py
+++ baselabwatch/views/baseviews.py
@@ -5,5 +5,12 @@ contexts relating to user:
     school
 """
 
-from django.views.generic import View
+from django.views.generic import View, TemplateView 
 
+class DashboardBase(TemplateView):
+    template_name = 'base/_base.html'
+
+    def post(self, request, *args, **kwargs):
+        "Handle post requests."
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
