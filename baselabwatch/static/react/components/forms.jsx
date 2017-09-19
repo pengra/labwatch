7500072433b6b15ad111d097a81217bf70e24c31
qwergram <npengra317@gmail.com>
2017-09-19 10:09:29 -0700

    input/textarea/checkbox renders created

--- baselabwatch/static/react/components/forms.jsx
+++ baselabwatch/static/react/components/forms.jsx
@@ -25,8 +25,9 @@ class FormGroup extends React.Component {
     // placeholder : placeholder of content
     // type : type of form
       // if type == select: 
-        // options: [["human friendly name", "value"] ... ]
+        // options: [["human friendly name", value] ... ]
         // value: default value (non human friendly name)
+        // multiple: true/false (multiple select?)
     // value : value of content
     // validationLevel : 0 = fail 1 = success 2 = nothing 
     // required?
@@ -55,7 +56,7 @@ class FormGroup extends React.Component {
       disabled={this.props.disabled || false}
       readonly={this.props.readonly || false}
       value={this.props.value || ""}
-      palceholder={this.props.placeholder || ""}
+      placeholder={this.props.placeholder || ""}
       name={this.props.name}
     />
   }
@@ -63,7 +64,48 @@ class FormGroup extends React.Component {
     return commonInputBuild();
   }
   selectInput() {
-
+    const renderChoices = this.props.options.map((item, index) => <option key={index} value={item[1]}>{item[0]}</option>)
+    return (
+      <select 
+        className="form-control" 
+        multiple={this.props.multiple}
+        required={this.props.required || false}
+        disabled={this.props.disabled || false}
+        readonly={this.props.readonly || false}
+        value={this.props.value || ""}
+        name={this.props.name}
+      >
+        {renderChoices}
+      </select>
+    )
+  }
+  textAreaInput() {
+    return (
+      <textarea 
+        className="form-control"
+        required={this.props.required || false}
+        disabled={this.props.disabled || false}
+        readonly={this.props.readonly || false}
+        placeholder={this.props.placeholder || ""}
+        name={this.props.name}
+      >
+        {this.props.value || ""}
+      </textarea>
+    )
+  }
+  checkboxInput() {
+    return (
+      <div className="form-check">
+        <input 
+          type="checkbox" 
+          required={this.props.required || false}
+          disabled={this.props.disabled || false}
+          readonly={this.props.readonly || false}
+          placeholder={this.props.placeholder || ""}
+          name={this.props.name}
+        />
+      </div>
+    )
   }
   render() {
     return (
