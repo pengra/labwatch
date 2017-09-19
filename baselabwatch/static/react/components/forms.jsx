c6c8f8b5302549ba48c76d21b2e0aeb6843afec4
qwergram <npengra317@gmail.com>
2017-09-19 11:56:22 -0700

    attempt rewrite w/ docs

--- baselabwatch/static/react/components/forms.jsx
+++ baselabwatch/static/react/components/forms.jsx
@@ -1,4 +1,3 @@
-
 class Form extends React.Component {
   // props:
     // url : url to OPTIONS to find form contents
@@ -7,27 +6,54 @@ class Form extends React.Component {
     super();
 
     this.state = {
-      form: "loading form..."
+      form: "loading form...",
+      formData: {}
     }
 
     this.ajaxSuccess = this.ajaxSuccess.bind(this);
     this.ajaxFail = this.ajaxFail.bind(this);
+    this.ajaxSubmit = this.ajaxSubmit.bind(this);
+    this.handleInputUpdate = this.handleInputUpdate(this);
     this.getFormData = this.getFormData.bind(this);
     this.loadForm = this.loadForm.bind(this);
   }
   
   // ajax/form submission
+  // OVERLOAD THESE FUNCTIONS
   ajaxSuccess() {}
   ajaxFail() {}
-  
+  ajaxSubmit() {}
+
+  // inputUpdate
+  handleInputUpdate(inputForm) {
+
+  }
+
   // loading form
   componentDidMount() {
     this.getFormData();
   }
-  loadForm(data) {
-    console.log(data)
+  loadForm(formOptions) {
+    const thisForm = formOptions.actions;
+    let formRender = [];
+    if ('POST' in thisForm) {
+      for (let key in thisForm) {
+        formRender.push(
+          <FormGroup 
+            type={thisForm[key].type}
+            required={thisForm[key].required}
+            disabled={thisForm[key].read_only}
+            label={thisForm[key].label}
+            key={key}
+          />
+        )
+      }
+    } else {
+      formRender = <div>Form has no POST method!</div>
+    }
+    console.log(formOptions)
     this.setState({
-      form: data
+      form: formRender
     });
   }
   getFormData() {
@@ -40,13 +66,38 @@ class Form extends React.Component {
 
   // rendering
   render() {
-    return (<div>{this.state.form}</div>)
+    return (<form>{this.state.form}</form>)
   }
 }
 
+// Read:
+// https://facebook.github.io/react/docs/forms.html
 class FormGroup extends React.Component {
   // props:
-    // input : one of the inputs defined below
+    // type : string representation of one of the inputs defined below
+    // label : what is this form?
+    // invalidFeedback : error message if they left it blank
+  // Input Specific Props:
+    // name : name of form
+    // placeholder : placeholder of content
+    // type : type of form
+      // if type == select: 
+        // options: [["human friendly name", value] ... ]
+        // value: default value (non human friendly name)
+        // multiple: true/false (multiple select?)
+    // value : value of content
+    // validationLevel : 0 = fail 1 = success 2 = nothing 
+    // required?
+    // disabled?
+    // readonly?
+  constructor() {
+    super()
+  }
+}
+
+class _FormGroup extends React.Component {
+  // props:
+    // type : string representation of one of the inputs defined below
     // label : what is this form?
     // invalidFeedback : error message if they left it blank
   // Input Specific Props:
@@ -81,12 +132,11 @@ class FormGroup extends React.Component {
     return <input 
       className={inputClass}
       type={this.props.type} 
-      required={this.props.required || false}
-      disabled={this.props.disabled || false}
-      readonly={this.props.readonly || false}
-      value={this.props.value || ""}
-      placeholder={this.props.placeholder || ""}
-      name={this.props.name}
+      required={this.props.required}
+      disabled={this.props.disabled}
+      readOnly={this.props.readonly}
+      value={this.props.value}
+      placeholder={this.props.placeholder}
     />
   }
   textInput() {
@@ -98,10 +148,10 @@ class FormGroup extends React.Component {
       <select 
         className="form-control" 
         multiple={this.props.multiple}
-        required={this.props.required || false}
-        disabled={this.props.disabled || false}
-        readonly={this.props.readonly || false}
-        value={this.props.value || ""}
+        required={this.props.required}
+        disabled={this.props.disabled}
+        readonly={this.props.readonly}
+        value={this.props.value}
         name={this.props.name}
       >
         {renderChoices}
@@ -141,8 +191,8 @@ class FormGroup extends React.Component {
       <div className="form-group row">
         <label className="col-sm-2 col-form-label">{this.props.label}</label>
         <div className="col-sm-10">
-          {input}
-          <div class="invalid-feedback">
+          {this.commonInputBuild()}
+          <div className="invalid-feedback">
             {this.props.invalidFeedback || "Invalid value."}
           </div>
         </div>
