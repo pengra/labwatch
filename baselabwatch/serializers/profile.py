e6b50fd2fa0f0d06c9386469b40d62c84674949d
qwergram <npengra317@gmail.com>
2017-09-20 11:57:59 -0700

    make certain fields read_only and set react_data a part of meta data

--- baselabwatch/serializers/profile.py
+++ baselabwatch/serializers/profile.py
@@ -8,18 +8,26 @@ resolution = URLResolution(Profile)
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     "Serializer for profile."
     url = serializers.HyperlinkedIdentityField(view_name=resolution.resolve("profile-detail"))
-    user = serializers.HyperlinkedRelatedField(source="user.pk", view_name=resolution.resolve('user-detail'), queryset=User.objects.all())
-    school = serializers.HyperlinkedRelatedField(source="school.pk", view_name='school-detail', queryset=School.objects.all())
+    user = serializers.HyperlinkedRelatedField(source="user.pk", view_name=resolution.resolve('user-detail'), read_only=True)
+    school = serializers.HyperlinkedRelatedField(source="school.pk", view_name='school-detail', read_only=True)
+    engineer = serializers.BooleanField(read_only=True)
+    librarian = serializers.BooleanField(read_only=True)
+    techsavy = serializers.BooleanField(read_only=True)
+
 
     class Meta:
         model = Profile
-        fields = (
-            'url',
-            'user',
-            'school',
-            'engineer',
-            'librarian',
-            'techsavy',
-            'beta_tester'
-        )
+        react_data = {
+            'url': {},
+            'user': {},
+            'school': {},
+            'engineer': {},
+            'librarian': {},
+            'techsavy': {},
+            'beta_tester': {
+                'label': "You'll get access to new but potentially buggy features."
+            },
+        }
+        fields = tuple(react_data.keys())
+        
         
