ce0bc1368d7580fc2865dbcfca3329cfeeb5ab70
qwergram <npengra317@gmail.com>
2017-09-20 10:44:07 -0700

    utilize resolver

--- baselabwatch/serializers/profile.py
+++ baselabwatch/serializers/profile.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from baselabwatch.models import Profile, School
+from baselabwatch.util.urls import URLResolution
+
+resolution = URLResolution(Profile)
 
 class ProfileSerializer(serializers.HyperlinkedModelSerializer):
     "Serializer for profile."
-    user = serializers.HyperlinkedRelatedField(source="user.pk", view_name='user-detail', queryset=User.objects.all())
+    url = serializers.HyperlinkedIdentityField(view_name=resolution.resolve("profile-detail"))
+    user = serializers.HyperlinkedRelatedField(source="user.pk", view_name=resolution.resolve('user-detail'), queryset=User.objects.all())
     school = serializers.HyperlinkedRelatedField(source="school.pk", view_name='school-detail', queryset=School.objects.all())
 
     class Meta:
@@ -18,3 +22,4 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
             'techsavy',
             'beta_tester'
         )
+        
