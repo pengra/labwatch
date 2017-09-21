96134b0765362c64eb922643b4efd38c1931c5ef
qwergram <npengra317@gmail.com>
2017-09-21 09:40:51 -0700

    additional operations on other objects after school created

--- baselabwatch/api/school.py
+++ baselabwatch/api/school.py
@@ -11,3 +11,9 @@ class SchoolViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolSerializer
     permission_classes = (IsAdminUser,)
     # renderer_classes = [JSONRenderer]
+
+    def perform_create(self, serializer):
+        school = serializer.save(primary_contact=self.request.user)
+        profile = self.request.user.profile
+        profile.school = school
+        profile.save()
