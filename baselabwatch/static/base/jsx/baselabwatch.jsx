908cabcb021fb0c9650042ee826c006637fbda2a
qwergram <npengra317@gmail.com>
2017-09-23 16:49:39 -0700

    limits page

--- baselabwatch/static/base/jsx/baselabwatch.jsx
+++ baselabwatch/static/base/jsx/baselabwatch.jsx
@@ -6,47 +6,98 @@ class SchoolAdminForm extends Form {
   // Overload methods:
   renderForm = () => {
     const formData = this.state.formData;
-    let proceed = true;
-    const length = Object.keys(formData).length;
-
-    Object.keys(formData).map((k, i) => {proceed = ("options" in formData[k] && proceed)})
-
-    if (proceed && length > 0) {
-      return (
-      <div>
-        <TextInput 
-          name="name" 
-          formData={formData.name} 
-          onChange={this.onChange}
-          errors={this.state.errors.name}
-          key={"name"}
-        />
-        <div style={{height: 100}}>
-          <img src={formData.school_image.value} height="100%"/>
-        </div>
-        <TextInput 
-          name="school_image" 
-          formData={formData.school_image} 
-          onChange={this.onChange}
-          errors={this.state.errors.school_image}
-          key={"school_image"}
-        />
-        <TextInput 
-          name="auth_code" 
-          formData={formData.auth_code} 
-          onChange={this.onChange}
-          key={"auth_code"}
-        />
-        <SubmitInput label="Save" success={this.state.success} fail={this.state.fail}/>
+    return (
+    <div>
+      <TextInput 
+        name="name" 
+        formData={formData.name} 
+        onChange={this.onChange}
+        errors={this.state.errors.name}
+        key={"name"}
+      />
+      <div style={{height: 100}}>
+        <img src={formData.school_image.value} height="100%"/>
       </div>
-      )
-    } else {
-      return null;
-    }
+      <TextInput 
+        name="school_image" 
+        formData={formData.school_image} 
+        onChange={this.onChange}
+        errors={this.state.errors.school_image}
+        key={"school_image"}
+      />
+      <TextInput 
+        name="auth_code" 
+        formData={formData.auth_code} 
+        onChange={this.onChange}
+        key={"auth_code"}
+      />
+      <SubmitInput label="Save" success={this.state.success} fail={this.state.fail}/>
+    </div>
+    )
   }
 }
 
-class SchoolPageContent extends React.Component {
+class LimitsPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  componentDidMount = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/base/api/v1/subscriptions/' + subscriptionID + '/',
+    })
+    .done((data) => {
+      this.setState(data)
+    })
+    .fail((data) => {
+    });
+  }
+  render = () => {
+    return (
+      <div style={{marginTop: "2%"}}> 
+        <div className="row" style={{display: "flex", alignItems: "center", marginTop: "2%"}}>
+          <div className="col-2" style={{textAlign: "right"}}>
+            <div>
+              <strong>Student IDs Available:</strong>
+            </div>
+          </div>
+          <div className="col-10">
+            <ProgressBar 
+              percentage={100 - ((this.state.current_student_ids * 100) / this.state.max_student_ids)} 
+              type="success" 
+              content={"" + (this.state.max_student_ids - this.state.current_student_ids) + " IDs Available"}
+            />
+          </div>
+        </div>
+        <div className="row" style={{marginTop: "2%"}}>
+          <div className="col-3">
+            <div className="card text-white bg-success mb-3">
+              <div className="card-header">Student Rewards</div>
+              <div className="card-body">
+                <h4 className="card-title">You're signed up for Student Rewards</h4>
+                <p className="card-text">Students will see rewards on logins and at special URLs.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card text-white bg-success mb-3">
+              <div className="card-header">Data Intelligence</div>
+              <div className="card-body">
+                <h4 className="card-title">You're signed up for Data Intelligence</h4>
+                <p className="card-text">LabWatch will recomend actions based on historical data.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class PageContent extends React.Component {
   handleSubmit = (event) => {
    
   }
@@ -55,12 +106,12 @@ class SchoolPageContent extends React.Component {
       <main className="tab-content">
         <div className="row">
           <div className="col-12">
-            <h2>School Administration</h2>
+            <h2>{this.props.title}</h2>
           </div>
         </div>
         <div className="row">
           <div className="col-12">
-            <SchoolAdminForm url={"http://127.0.0.1:8000/base/api/v1/schools/" + schoolID + '/'} id="school-admin-form"/>
+            {this.props.content}
           </div>
         </div>
       </main>
@@ -69,48 +120,37 @@ class SchoolPageContent extends React.Component {
 }
 
 class MainContent extends LabWatchWebsite {
-  constructor() {
-    super();
-
-  }
   renderSchoolPage = () => {
     const schoolPageContent = [
-      ["Settings", 
-        <SchoolPageContent />
+      [
+        "Settings", 
+        <PageContent title="School Administration" content={<SchoolAdminForm url={"/base/api/v1/schools/" + schoolID + '/'} id="school-admin-form"/>}/>
       ],
-      ["Limits", 
-      <main className="tab-content">
-        <div className="row">
-          <div className="col-12">
-            <h2>Subscription Administration</h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <Form url={"http://127.0.0.1:8000/base/api/v1/subscriptions/" + subscriptionID} />
-          </div>
-        </div>
-      </main>
+      [
+        "Limits", 
+        <PageContent title="Subscription Administration" content={<LimitsPage />} />
       ],
-      ["Payment", 
-      <main className="tab-content">
-        <div className="row tab-header">
-          <div className="col-12">
-            <h2>Payment <span className="badge badge-success">Free Beta</span></h2>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12">
-            <div className="alert alert-primary">
-              <h4 className="alert-heading">Free Beta!</h4>
-              LabWatch is currently a free beta. We believe it'd be unfair to charge
-              schools for a service that still has bugs. All participating schools are
-              granted <strong>unlimited</strong> resources at no cost. Schools will be notified
-              ahead of time when the free beta comes to an end. 
+      [
+        "Payment", 
+        <main className="tab-content">
+          <div className="row tab-header">
+            <div className="col-12">
+              <h2>Payment <span className="badge badge-success">Free Beta</span></h2>
             </div>
           </div>
-        </div>
-      </main>],
+          <div className="row">
+            <div className="col-12">
+              <div className="alert alert-primary">
+                <h4 className="alert-heading">Free Beta!</h4>
+                LabWatch is currently a free beta. We believe it'd be unfair to charge
+                schools for a service that still has bugs. All participating schools are
+                granted <strong>unlimited</strong> resources at no cost. Schools will be notified
+                ahead of time when the free beta comes to an end. 
+              </div>
+            </div>
+          </div>
+        </main>
+      ],
     ];
     return (<div>
       <TabbedPage tabs={schoolPageContent} />
@@ -129,8 +169,8 @@ class MainContent extends LabWatchWebsite {
   renderProfilePage = () => {
     return (<div>
       <h1>Edit your Profile</h1>
-      <Form url={"http://127.0.0.1:8000/base/api/v1/profiles/" + profileID} />
-      <Form url={"http://127.0.0.1:8000/base/api/v1/users/" + userID} />
+      <Form url={"/base/api/v1/profiles/" + profileID} />
+      <Form url={"/base/api/v1/users/" + userID} />
     </div>)
   }
   renderMainPage = () => {
