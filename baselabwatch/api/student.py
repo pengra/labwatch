cc420bf18c7f33cf0d473d673fda6a23d2de951a
qwergram <npengra317@gmail.com>
2017-09-24 14:29:06 -0700

    add search capabilities to student

--- baselabwatch/api/student.py
+++ baselabwatch/api/student.py
@@ -1,15 +1,20 @@
 from baselabwatch.models import Student
 from baselabwatch.serializers import StudentSerializer
-from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
+from rest_framework.filters import SearchFilter
 
 class StudentViewSet(viewsets.ModelViewSet):
     "Viewsets for Students."
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = (IsAdminUser,)
-    # renderer_classes = [JSONRenderer]
+    filter_backends = (SearchFilter, )
+    search_fields = (
+        'first_name', 'last_name', 'nick_name',
+        'student_id', 'teacher', 'grade',
+        'email',
+    )
 
     def perform_create(self, serializer):
         serializer.save(school=self.request.user.profile.school)
