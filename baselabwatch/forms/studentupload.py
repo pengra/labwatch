711c9926630d77bea0a12dc70809be97acfd3be6
qwergram <npengra317@gmail.com>
2017-09-26 11:03:38 -0700

    add xml fields

--- baselabwatch/forms/studentupload.py
+++ baselabwatch/forms/studentupload.py
@@ -7,3 +7,10 @@ class XMLFileUploadForm(forms.Form):
     "Form for batch student XML uploads."
     spreadsheet = forms.FileField()
     dupe_action = forms.CharField()
+    studentid = forms.CharField()
+    fname = forms.CharField()
+    lname = forms.CharField()
+    grade = forms.CharField(required=False)
+    teacher = forms.CharField(required=False)
+    nickname = forms.CharField(required=False)
+    email = forms.CharField(required=False)
