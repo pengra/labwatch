13136de52ffeaf0364eb04413bc276807e25fe5e
qwergram <npengra317@gmail.com>
2017-09-26 19:54:05 -0700

    file upload form

--- baselabwatch/static/base/jsx/baselabwatch.jsx
+++ baselabwatch/static/base/jsx/baselabwatch.jsx
@@ -180,16 +180,110 @@ class UploadPageContent extends React.Component {
   constructor() {
     super();
     this.state = {
-      xmlUploaded: false, // has the user selected an XML file?
-      exampleData: {},    // show the user some example data e.g. "are these student usernames? last names?"
+      formId: 'uploadstudentsform',
+      uploading: false,
     }
   }
   handleSubmit = (event) => {
+    // https://stackoverflow.com/questions/2320069/jquery-ajax-file-upload
+    event.preventDefault();
 
+    this.setState({
+      uploading: true
+    })
+
+    if (!window.FormData) {
+      alert("You're using an old browser. Uploads will not work properly.");
+    }
+
+    let formData = new FormData();
+
+    formData.append("spreadsheet", $('#fileUploadInput')[0].files[0])
+    formData.append("dupe_action", $('#dupe_actionColumn').val())
+    formData.append("studentid", $('#studentidColumn').val())
+    formData.append("fname", $('#fnameColumn').val())
+    formData.append("lname", $('#lnameColumn').val())
+    formData.append("grade", $('#gradeColumn').val())
+    formData.append("teacher", $('#teacherColumn').val())
+    formData.append("nickname", $('#nicknameColumn').val())
+    formData.append("email", $('#emailColumn').val())
+
+    $.ajax({
+      method: 'POST',
+      url: this.props.url,
+      data: formData,
+      cache       : false,
+      contentType : false,
+      processData : false,
+    }).done((data) => {
+      this.successfulUpload(data);
+    }).fail((data) => {
+      this.failedUpload(data.responseJSON);
+    })
   }
+  
+  successfulUpload = (data) => {
+    this.setState({
+      uploading: false,
+      message: (
+        "Uploaded " + data.new + " student(s). Found " + 
+        data.dupes + " duplicates and " + (data.overwrite ? "overwrote " : "ignored ") + "them. " +
+        data.fails + " students failed to upload."),
+      success: true,
+      fail: false
+    });
+    console.log(data);
+  }
+
+  failedUpload = (data) => {
+    this.setState({
+      uploading: false,
+      message: (
+        "Please make sure you've filled out the entire form."
+      ),
+      fail: true,
+      success: false
+    });
+    console.log(data);
+  }
+
+  statusMessage = () => {
+    if (this.state.success || this.state.fail) {
+      return (
+        <div className="row">
+          <div className="col-12">
+            <div className={"alert alert-" + (this.state.success ? "success" : "danger")}>
+              {this.state.message}
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+    
+  }
+
+  renderLoading = () => {
+    if (this.state.uploading) {
+      return (
+        <div className="row">
+          <div className="col-12">
+            <img src="/static/images/loading1.gif" style={{maxHeight: 100}} /> 
+            <span style={{marginLeft: 2}}> Uploading &amp; Processing your file... </span>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
   render = () => {
     return (
-      <form onSubmit={this.handleSubmit} enctype="multipart/form-data">
+      <form onSubmit={this.handleSubmit} id={this.state.formId} encType="multipart/form-data">
+        {this.renderLoading()}
+        {this.statusMessage()}
         <div className="row">
           <div className="col-12">
             <FileInput />
@@ -199,7 +293,7 @@ class UploadPageContent extends React.Component {
           <div className="col-12">
             <label>What do you want to do about duplicates?</label>
             <div className="form-group">
-              <select className="form-control" name="dupe_action">
+              <select className="form-control" name="dupe_action" id='dupe_actionColumn'>
                 <option value="ignore">Ignore duplicate student</option>
                 <option value="overwrite">Overwrite existing student with upload</option>
               </select>
@@ -209,30 +303,30 @@ class UploadPageContent extends React.Component {
 
         <div className="row">
           <div className="form-group col-xs-12 col-lg-4">
-            <label htmlFor="studentIDColumn">Enter the tag name containing student IDs</label>
-            <input type="text" name="studentid" className="form-control" id="studentIDColumn" aria-describedby="studentIDhelp" defaultValue="districtID"/>
+            <label htmlFor="studentidColumn">Enter the tag name containing student IDs</label>
+            <input type="text" name="studentid" className="form-control" id="studentidColumn" aria-describedby="studentIDhelp" defaultValue="districtID"/>
             <small id="studentIDhelp" className="form-text text-muted">In the example, it'd be "districtID" <strong>(required)</strong></small>
           </div>
           <div className="form-group col-xs-12 col-lg-4">
-            <label htmlFor="studentFirstNameColumn">Enter the tag name containing Student First Names</label>
-            <input type="text" name="fname" className="form-control" id="studentFirstNameColumn" aria-describedby="studentFirstNamehelp" defaultValue="nameFirst"/>
+            <label htmlFor="fnameColumn">Enter the tag name containing Student First Names</label>
+            <input type="text" name="fname" className="form-control" id="fnameColumn" aria-describedby="studentFirstNamehelp" defaultValue="nameFirst"/>
             <small id="studentFirstNamehelp" className="form-text text-muted">In the example, it'd be "nameFirst" <strong>(required)</strong></small>
           </div>
           <div className="form-group col-xs-12 col-lg-4">
-            <label htmlFor="studentLastNamesColumn">Enter the tag name containing Student Last Names</label>
-            <input type="text" name="lname" className="form-control" id="studentLastNamesColumn" aria-describedby="studentLastNameshelp" defaultValue="nameLast"/>
+            <label htmlFor="lnameColumn">Enter the tag name containing Student Last Names</label>
+            <input type="text" name="lname" className="form-control" id="lnameColumn" aria-describedby="studentLastNameshelp" defaultValue="nameLast"/>
             <small id="studentLastNameshelp" className="form-text text-muted">In the example, it'd be "nameLast" <strong>(required)</strong></small>
           </div>
         </div>
         <div className="row">
           <div className="form-group col-xs-12 col-lg-4">
-            <label htmlFor="gradeLevelColumn">Enter the tag name containing Student Grade level</label>
-            <input type="text" name="grade" className="form-control" id="gradeLevelColumn" aria-describedby="gradeLevelhelp" defaultValue="gradeLevel"/>
+            <label htmlFor="gradeColumn">Enter the tag name containing Student Grade level</label>
+            <input type="text" name="grade" className="form-control" id="gradeColumn" aria-describedby="gradeLevelhelp" defaultValue="gradeLevel"/>
             <small id="gradeLevelhelp" className="form-text text-muted">In the example, it'd be "gradeLevel" (optional)</small>
           </div>
           <div className="form-group col-xs-12 col-lg-4">
-            <label htmlFor="homeroomColumn">Enter the tag name containing Student's Teacher</label>
-            <input type="text" name="teacher" className="form-control" id="homeroomColumn" aria-describedby="homeroomhelp" defaultValue="homeroom"/>
+            <label htmlFor="teacherColumn">Enter the tag name containing Student's Teacher</label>
+            <input type="text" name="teacher" className="form-control" id="teacherColumn" aria-describedby="homeroomhelp" defaultValue="homeroom"/>
             <small id="homeroomhelp" className="form-text text-muted">In the example, it'd be "homeroom" (optional)</small>
           </div>
           <div className="form-group col-xs-12 col-lg-4">
@@ -329,7 +423,9 @@ class MainContent extends LabWatchWebsite {
         <PageContent
           title="Upload Student Spreadsheet"
           content={
-            <UploadPageContent />
+            <UploadPageContent 
+              url='/base/api/v1/upload/'
+            />
           }
         />
       ],
