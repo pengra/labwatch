7649cd79706abc2a5f1975ddadf21ae4a742d0a9
qwergram <npengra317@gmail.com>
2017-09-27 11:56:38 -0700

    unique constraint on serializer

--- baselabwatch/serializers/student.py
+++ baselabwatch/serializers/student.py
@@ -1,6 +1,7 @@
 from baselabwatch.models import Student, School
 from rest_framework import serializers
 from baselabwatch.util.urls import URLResolution
+from rest_framework.validators import UniqueValidator
 
 resolution = URLResolution(Student)
 
@@ -9,8 +10,10 @@ class StudentSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name=resolution.resolve('student-detail'))
     school = serializers.HyperlinkedRelatedField(source="school.pk", view_name=resolution.resolve('school-detail'), read_only=True)
     signed_in = serializers.BooleanField(read_only=True)
-    student_id = serializers.CharField(label='Student ID', help_text='This number must be unique.')
-    nick_name = serializers.CharField(help_text='Should the user forget his/her ID, they can alternatively type in a username or nickname.', style={'placeholder': 'e.g. pengrnor000'})
+    student_id = serializers.IntegerField(label='Student ID', help_text='A unique numerical student identifier.',
+        validators=[UniqueValidator(queryset=Student.objects.all())]
+    )
+    nick_name = serializers.CharField(help_text='Should the user forget his/her ID, they can alternatively type in a username or nickname.', style={'placeholder': 'e.g. pengrnor000'}, required=False)
 
     class Meta:
         model = Student
