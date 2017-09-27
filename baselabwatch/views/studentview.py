98de3cee0f0407873cc9c794f4d8773d6bd8bb40
qwergram <npengra317@gmail.com>
2017-09-27 11:56:49 -0700

    new create student view

--- baselabwatch/views/studentview.py
+++ baselabwatch/views/studentview.py
@@ -1,5 +1,16 @@
 from baselabwatch.views import DashboardBase
+from baselabwatch.serializers import StudentSerializer
 
 class StudentView(DashboardBase):
     template_name = 'base/studentadmin.html'
     current_app = 'baselabwatch'
+    serializer = StudentSerializer
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     if context['serializer']:
+    #         context['serializer'] = context['serializer'](
+    #             instance=self.request.user.profile.school,
+    #             context={'request': self.request}
+    #         )
+    #     return context
