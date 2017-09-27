9903fa911cff5e1fa9e7622471dea12443ede0eb
qwergram <npengra317@gmail.com>
2017-09-27 08:11:01 -0700

    add short_name + name field to school model

--- baselabwatch/models/school.py
+++ baselabwatch/models/school.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User
 
 class School(models.Model):
     "An object representing a single school."
-    name = models.CharField(max_length=255, unique=True)
+    short_name = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=255, unique=True, blank=True)
     primary_contact = models.OneToOneField(User, related_name='primary_contact_for', null=True)
     auth_code = models.CharField(max_length=32, unique=True)
     school_image = models.URLField()
 
+    logger_access = models.BooleanField(blank=True, default=False)
+
     def __str__(self):
-        return self.name
+        return self.short_name
