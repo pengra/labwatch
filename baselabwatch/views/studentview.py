c4ffeb2ef21ffa606d934af7cbb94c7c806dec2b
qwergram <npengra317@gmail.com>
2017-09-27 21:57:40 -0700

    added form xml upload handling

--- baselabwatch/views/studentview.py
+++ baselabwatch/views/studentview.py
@@ -1,7 +1,22 @@
 from baselabwatch.views import DashboardBase
 from baselabwatch.serializers import StudentSerializer
+from baselabwatch.forms import XMLFileUploadForm
+from baselabwatch.views import XMLUploadView
+from django.shortcuts import redirect
+from django.urls import reverse
+from json import loads
+from urllib.parse import urlencode
 
 class StudentView(DashboardBase):
     template_name = 'base/studentadmin.html'
     current_app = 'baselabwatch'
     serializer = StudentSerializer
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['upload_form'] = XMLFileUploadForm
+        return context
+
+    def post(self, *args, **kwargs):
+        uploadResponse = XMLUploadView.post(self, *args, **kwargs)
+        return redirect(reverse('baselabwatch:student') + '?tab=upload&upload=true&' + urlencode(loads(uploadResponse.content.decode())))
