02f09c8e9c55b22640ace3ffd7e918663f14a6e0
qwergram <npengra317@gmail.com>
2017-09-30 10:19:05 -0700

    add help_texts

--- logger/serializers/kiosk.py
+++ logger/serializers/kiosk.py
@@ -10,6 +10,8 @@ class KioskSerializer(serializers.HyperlinkedModelSerializer):
     "Serializer for profile."
     school = serializers.HyperlinkedRelatedField(source="school.pk", view_name=resolution.resolve('school-detail'), read_only=True)
     poll = serializers.HyperlinkedRelatedField(source='poll.pk', view_name=resolution2.resolve('pollquestion-detail'), read_only=True)
+    name = serializers.CharField(help_text="Kiosk Title")
+    auth_code = serializers.CharField(label="Kiosk Code", help_text="A code for quick kiosk access", read_only=True)
 
     class Meta:
         model = Kiosk
@@ -18,5 +20,5 @@ class KioskSerializer(serializers.HyperlinkedModelSerializer):
             'name',
             'school',
             'auth_code',
-            'poll'
+            'poll',
         )
