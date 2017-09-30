6669e2976a9857c906d1c4e110caa9adda28ff7d
qwergram <npengra317@gmail.com>
2017-09-30 14:06:05 -0700

    allow poll_question and choices to be blank

--- logger/forms/kioskpollform.py
+++ logger/forms/kioskpollform.py
@@ -3,6 +3,6 @@ from django import forms
 class KioskForm(forms.Form):
     kiosk_pk = forms.IntegerField()
     name = forms.CharField()
-    poll_question = forms.CharField()
-    poll_choices = forms.CharField()
+    poll_question = forms.CharField(required=False)
+    poll_choices = forms.CharField(required=False)
     
