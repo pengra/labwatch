1384f33c8881cfecaa219e34004f902e67c73e64
qwergram <npengra317@gmail.com>
2017-09-30 10:19:31 -0700

    resolve poll urls

--- logger/serializers/poll.py
+++ logger/serializers/poll.py
@@ -1,25 +1,28 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from logger.models import PollQuestion, PollChoice
+from baselabwatch.util.urls import URLResolution
+
+resolution = URLResolution(PollQuestion)
 
 class PollQuestionSerializer(serializers.HyperlinkedModelSerializer):
     "Serializer for poll questions."
     class Meta:
         model = PollQuestion
         fields = (
-            'url',
+            'pk',
             'question_text'
         )
 
 
 class PollChoiceSerializer(serializers.HyperlinkedModelSerializer):
     "Serializer of poll choices"
-    question = serializers.HyperlinkedRelatedField(source="question.pk", view_name="pollquestion-detail", queryset=PollQuestion.objects.all())
+    question = serializers.HyperlinkedRelatedField(source="question.pk", view_name=resolution.resolve("pollquestion-detail"), queryset=PollQuestion.objects.all())
 
     class Meta:
         model = PollChoice
         fields = (
-            'url',
+            'pk',
             'question',
             'choice_text',
             'votes'
