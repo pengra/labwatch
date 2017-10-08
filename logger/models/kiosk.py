0102e32b9bd0c551756e5764c306b2252db388fb
qwergram <npengra317@gmail.com>
2017-10-08 16:43:13 -0700

    utilize UUID field

--- logger/models/kiosk.py
+++ logger/models/kiosk.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 from logger.models import PollQuestion
 from baselabwatch.models import School
 
@@ -6,7 +7,7 @@ class Kiosk(models.Model):
     "An object representing a kiosk available for a school to use."
     name = models.CharField(max_length=255)
     school = models.ForeignKey(School)
-    auth_code = models.CharField(max_length=32, unique=True)
+    auth_code = models.UUIDField(max_length=32, unique=True, default=uuid.uuid4)
     poll = models.ForeignKey(PollQuestion, blank=True, null=True)
 
     def __str__(self):
