56cafd12042d290ffce9718e152c56e116b2ef78
qwergram <npengra317@gmail.com>
2017-10-09 19:32:46 -0700

    include in views module

--- logger/views/__init__.py
+++ logger/views/__init__.py
@@ -1,3 +1,4 @@
 from logger.views.overview import OverviewView
 from logger.views.kioskview import KioskView
 from logger.views.reportexport import ReportExportView
+from logger.views.clientview import ClientView
