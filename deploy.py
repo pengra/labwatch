a3b3a2c1193c60523f011e7ea372f77b2e2f9f35
qwergram <npengra317@gmail.com>
2017-10-13 10:02:18 -0700

    deploy script v2

--- deploy.py
+++ deploy.py
@@ -1,11 +1,15 @@
 from django.core.management import execute_from_command_line
 from django.contrib.auth.models import User
-from os import getenv
+from os import getenv, environ
 
 if __name__ == '__main__':
     # run migrations
+    print('[DEPLOY]: settings DJANGO_SETTINGS_MODULE')
+    environ.setdefault("DJANGO_SETTINGS_MODULE", "labwatch.settings")
+    print('[DEPLOY]: running migrations')
     execute_from_command_line('migrate')
     if User.objects.filter(username=getenv('SUPERUSER_USERNAME', 'qwergram')).count() == 0:
+        print('[DEPLOY]: creating superuser')
         user = User(
             username=getenv('SUPERUSER_USERNAME', 'qwergram'),
             password=getenv('SUPERUSER_PASSWORD', 'default_p@ssword'),
