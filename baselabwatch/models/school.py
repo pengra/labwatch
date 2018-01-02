89e6ff58f0502a7d61a5214077fa33a79cde0d6d
qwergram <npengra317@gmail.com>
2018-01-02 03:59:55 -0800

    scramble to update

--- baselabwatch/models/school.py
+++ baselabwatch/models/school.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,8 +9,8 @@ class School(models.Model):
     short_name = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=255, unique=True, blank=True)
     primary_contact = models.OneToOneField(User, related_name='primary_contact_for', null=True)
-    auth_code = models.CharField(max_length=32, unique=True)
-    school_image = models.URLField()
+    auth_code = models.UUIDField(max_length=32, unique=True, default=uuid.uuid4)
+    school_image = models.URLField(default="https://imgur.com/5Zx4W1p.png")
 
     def __str__(self):
         return self.short_name
