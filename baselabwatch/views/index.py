bd7c15bec691e67980e41bdc4a3c24044831689d
Norton Pengra <npengra317@gmail.com>
2018-09-23 01:33:08 -0700

    allow posts

--- baselabwatch/views/index.py
+++ baselabwatch/views/index.py
@@ -2,3 +2,14 @@ from django.views.generic import View, TemplateView
 
 class IndexView(TemplateView):
     template_name = "base/coverpage.html"
+
+    def post(self, *args, **kwargs):
+        from django.core.mail import mail_admins
+        email = self.request.POST.get('email')
+        mail_admins(
+            '[sign up]',
+            'Someone signed up with: ' + email,
+        )
+        context = self.get_context_data(**kwargs)
+        context['signed_up'] = True
+        return self.render_to_response(context)
