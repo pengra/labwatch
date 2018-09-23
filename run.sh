b90e352cfb9711b7abf4176e1e67ed49156bc308
Norton Pengra <npengra317@gmail.com>
2018-09-23 03:15:38 -0700

    no input

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 . secrets.sh
 cd $SERVER_LOCATION
 source "env/bin/activate"
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 gunicorn --workers=3 -b 127.0.0.1:9998 labwatch.wsgi:application
 
 
