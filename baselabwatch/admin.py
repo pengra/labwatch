f52f8a50d488a6bfd6ca58d084b340ff5f9604bc
Norton Pengra <npengra317@gmail.com>
2018-10-14 17:17:17 -0700

    add student admin controls

--- baselabwatch/admin.py
+++ baselabwatch/admin.py
@@ -3,8 +3,17 @@ from baselabwatch import models
 
 # Register your models here.
 
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = (
+        'first_name',
+        'last_name',
+        'nick_name',
+        'student_id',
+    )
+    
+
 admin.site.register(models.Profile)
 admin.site.register(models.School)
-admin.site.register(models.Student)
+admin.site.register(models.Student, StudentAdmin)
 admin.site.register(models.Subscription)
 admin.site.register(models.UserReport)
