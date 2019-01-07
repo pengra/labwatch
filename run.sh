150c1d8b1d99ce0e0dad0f65e7b7d2cf3de7881f
Norton Pengra <npengra317@gmail.com>
2019-01-07 01:05:11 +0000

    reduce number of workers

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 # https://stackoverflow.com/questions/3004811/how-do-you-run-multiple-programs-in-parallel-from-a-bash-script
 . secrets.sh
 cd $SERVER_LOCATION
-source "env/bin/activate"
+source ".env/bin/activate"
 python manage.py collectstatic --no-input
-gunicorn --workers=3 -b 127.0.0.1:9998 labwatch.wsgi:application
+gunicorn --workers=2 -b 127.0.0.1:9998 labwatch.wsgi:application
 
 
 # gunicorn dashing.wsgi:application --workers=1 -b 127.0.0.1:9999 & celery -A dashing worker -l info && fg
